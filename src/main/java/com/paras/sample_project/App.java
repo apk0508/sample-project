4fc42d7dffb8b34219a132d2755e256a7da6bc99
paku272260 <paras.kumar@elektrobit.com>
2021-10-26 13:22:58 +0530

    second commit

--- src/main/java/com/paras/sample_project/App.java
+++ src/main/java/com/paras/sample_project/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("My Project");
+        System.out.println("My new Project");
+        
     }
 }

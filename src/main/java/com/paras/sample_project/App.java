5d5ca4d1eef71e505c929fbc2d09260527cb464c
apk0508 <52490608+apk0508@users.noreply.github.com>
2021-10-26 17:59:16 +0530

    Update App.java

--- src/main/java/com/paras/sample_project/App.java
+++ src/main/java/com/paras/sample_project/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("My new sample projectss");
+        System.out.println("My new sample project");
         
     }
 }

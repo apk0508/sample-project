e79c45663f57933e6c40439bf33499a85c6f729b
paku272260 <paras.kumar@elektrobit.com>
2021-10-29 09:39:00 +0530

    Updated App.java

--- src/main/java/com/paras/sample_project/App.java
+++ src/main/java/com/paras/sample_project/App.java
@@ -1,15 +1,20 @@
 package com.paras.sample_project;
 
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
+
 {
+	
+	private static final Logger LOG = Logger.getLogger(App.class.getName());
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("My new sample project");
+    	
+    	LOG.info("Hello World");
         
     }
 }

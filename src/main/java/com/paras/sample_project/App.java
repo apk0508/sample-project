2f09bd8669c65a80f6d4b578ab81a72ca5f06ee8
paku272260 <paras.kumar@elektrobit.com>
2021-10-29 09:58:49 +0530

    Add Junit in App

--- src/main/java/com/paras/sample_project/App.java
+++ src/main/java/com/paras/sample_project/App.java
@@ -2,6 +2,8 @@ package com.paras.sample_project;
 
 import java.util.logging.Logger;
 
+import org.junit.Test;
+
 import junit.framework.Assert;
 
 /**
@@ -13,9 +15,10 @@ public class App
 {
 	
 	private static final Logger LOG = Logger.getLogger(App.class.getName());
-    public static void main( String[] args )
+	
+	@Test
+    public void test()
     {
-    	
     	LOG.info("Hello World");
     	int a  =2 ;
     	if(a !=2 ) {
